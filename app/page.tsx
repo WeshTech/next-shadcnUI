a96ba176be49fb1d90aaaccce641bdc0d9150e27
WeshTech <davidwaweru2500@gmail.com>
2025-07-31 04:47:09 +0300

    avatar, darkmode, dropdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { DatabaseZap } from "lucide-react";
-
 export default function Home() {
-  return (
-    <div className="h-screen flex flex-col gap-5 items-center justify-center">
-      <p className="text-orange-500">You are in the home page</p>
-      <Button size="lg" className="rounded-full">
-        <DatabaseZap />
-        Optimize Database
-      </Button>
-    </div>
-  );
+  return <div></div>;
 }
