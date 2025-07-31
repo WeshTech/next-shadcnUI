1252266e3e0121c4691a9e0bf6a87a94c5c2b214
WeshTech <davidwaweru2500@gmail.com>
2025-07-31 13:25:06 +0300

    barchart

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div></div>;
-}
+import AppBarChart from "@/components/AppBarChart";
+
+const HomePage = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-gray-100 p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-gray-100 p-4 rounded-lg"></div>
+      <div className="bg-gray-100 p-4 rounded-lg"></div>
+      <div className="bg-gray-100 p-4 rounded-lg"></div>
+      <div className="bg-gray-100 p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"></div>
+      <div className="bg-gray-100 p-4 rounded-lg"></div>
+    </div>
+  );
+};
+
+export default HomePage;
